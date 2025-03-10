abee40ffb856a064100c3d1484a8f5e177b33b03
gapdapper <puri.pongsomboonsuk@gmail.com>
2025-03-10 23:37:14 +0700

    Task11 add button + mapping event

--- 713-frontend-participant/src/views/participant/EditView.vue
+++ 713-frontend-participant/src/views/participant/EditView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { toRefs } from 'vue';
 import type { Participant } from '@/types';
+import { useRouter } from 'vue-router';
 const props = defineProps<{ participant: Participant, id: string }>();
 const { participant } = toRefs(props);
+const router = useRouter();
+const edit = () => {
+    router.push({ name: 'participant-detail-view'});
+}
 </script>
 
 <template>
            <p>Edit participant here</p>
+    <button @click="edit">Edit</button>
 </template>
