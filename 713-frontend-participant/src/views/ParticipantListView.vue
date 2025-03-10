78b138db7dfb1938641ec386d0917a7770c19cdd
gapdapper <puri.pongsomboonsuk@gmail.com>
2025-03-10 11:19:38 +0700

    Task3 add v-if v-else

--- 713-frontend-participant/src/views/ParticipantListView.vue
+++ 713-frontend-participant/src/views/ParticipantListView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import ParticipantCard from '@/components/ParticipantCard.vue';
+const participants = ref([
+    {id: 1, name: 'Participant 1'},
+    {id: 2, name: 'Participant 2'},
+    {id: 3, name: 'Participant 3'}
+])
 </script>
 
 <template>
-    <div>
-        <ParticipantCard />
-        <ParticipantCard />
-        <ParticipantCard />
+    <div v-if="participants.length === 0">No participants</div>
+    <div v-else>
+        <ParticipantCard v-for="participant in participants" :key="participant.id" />
     </div>
 </template>
