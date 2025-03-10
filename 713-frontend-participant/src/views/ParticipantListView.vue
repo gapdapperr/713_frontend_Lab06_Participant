44cd2bf2b2d431b801b17a30df69a2e95295637b
gapdapper <puri.pongsomboonsuk@gmail.com>
2025-03-10 11:21:41 +0700

    Task3 back to v-for

--- 713-frontend-participant/src/views/ParticipantListView.vue
+++ 713-frontend-participant/src/views/ParticipantListView.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import ParticipantCard from '@/components/ParticipantCard.vue';
+import { ref } from 'vue'
+import ParticipantCard from '@/components/ParticipantCard.vue'
 const participants = ref([
-    {id: 1, name: 'Participant 1'},
-    {id: 2, name: 'Participant 2'},
-    {id: 3, name: 'Participant 3'}
+  { id: 1, name: 'Participant 1' },
+  { id: 2, name: 'Participant 2' },
+  { id: 3, name: 'Participant 3' },
 ])
 </script>
 
 <template>
-    <div v-if="participants.length === 0">No participants</div>
-    <div v-else-if="participants.length === 1">Only one participant</div>
-    <div v-else>
-        <ParticipantCard v-for="participant in participants" :key="participant.id" />
-    </div>
+  <div>
+    <ParticipantCard v-for="participant in participants" :key="participant.id" />
+  </div>
 </template>
