39e6d8b78371150f5ed8b2bb5754d58986d2959e
gapdapper <puri.pongsomboonsuk@gmail.com>
2025-03-11 13:51:09 +0700

    add css to event box in participant

--- 713-frontend-participant/src/views/participant/DetailView.vue
+++ 713-frontend-participant/src/views/participant/DetailView.vue
@@ -6,18 +6,38 @@ const { participant } = toRefs(props);
 </script>
 
 <template>
-    
+    <div class="participant-card">
         <h1>{{ participant.name }}</h1>
-        
         <p>{{ participant.email }}</p>
-        <div  v-for="events in participant.events" :key="events.id">
-            <p>{{ events.title}}</p>
-            <p>{{ events.time }}</p>
-            <p>{{ events.date }}</p>
-            <p>{{ events.location }}</p>
-            <p>{{ events.description }}</p>
-            <p>{{ events.petsAllowed }}</p>
+        <div  v-for="events in participant.events" :key="events.id" class="participant-event-box">
+            <p>Event {{ events.id }}</p>
+            <p>Title: {{ events.title}}</p>
+            <p>Time: {{ events.time }}</p>
+            <p>Date: {{ events.date }}</p>
+            <p>Location: {{ events.location }}</p>
+            <p>Description: {{ events.description }}</p>
+            <p>Pet Allowed: {{ events.petsAllowed }}</p>
         </div>
-    
+    </div>
     
 </template>
+
+<style scoped>
+.participant-card {
+    padding: 20px;
+  width: 350px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  margin: auto;
+}
+
+.participant-event-box {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin: auto;
+    margin-bottom: 20px;
+}
+</style>
